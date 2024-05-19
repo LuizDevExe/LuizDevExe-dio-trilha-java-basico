d85e63753f6afb862120030437b63a487db07a4f
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2024-05-19 04:16:37 -0300

    Desafio conta bancária dio

--- desafios/conta-banco/src/ContaTerminal.java
+++ desafios/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      Scanner scanner = new Scanner(System.in);
+
+        double balance = 231.89;
+
+        System.out.println("Digite seu nome");
+        String clientName = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencyId = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da sua conta !");
+        int accountId = scanner.nextInt();
+
+        System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencyId + ", conta "+ accountId + " e seu saldo " + balance  + " já está disponível para saque.");
+
+        scanner.close();
+
     }
 }
