e4270e67abdefdccc72dde96439a3f0aaf22058e
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2024-05-20 22:29:04 -0300

    controle de fluxo

---
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,10 @@
+public class CaixaEletronico {
+    public static void main(String[] args) throws Exception {
+        double balance = 25.0;
+        double withdrawValue = 26.0;
+
+        if ( withdrawValue <  balance)
+            balance = balance - withdrawValue;
+        System.out.println(balance);
+    }
+}
